f6e2c203e625daf84499ac46f616777d7a59e0c4
Ray-M05 <rachelmojena@gmail.com>
2025-05-31 20:50:22 -0400

    fix: debug

--- Ports_service/src/main.c
+++ Ports_service/src/main.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "port_utils.h"
 #include "port_scanner.h"
 
 int main(void) {
     GHashTable *tabla = inicializar_tabla_puertos();
+    if (tabla == NULL) {
+        fprintf(stderr, "ERROR: No se pudo inicializar la tabla de puertos.\n");
+        return EXIT_FAILURE;
+    }
+
     printf("🔍 Escaneando puertos 1–1024 con %d hilos...\n", NUM_HILOS);
     escanear_puertos(tabla, 1, 1024);
+
     g_hash_table_destroy(tabla);
-    return 0;
+    return EXIT_SUCCESS;
 }
